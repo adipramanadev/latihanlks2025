53990993b96cbbfa5b6f79face6174a8e7e1604e
Adipramana <herryprasetyo.dev@gmail.com>
2024-10-21 09:02:40 +0700

    first commit

---
+++ resources/views/admin/category/index.blade.php
@@ -0,0 +1,9 @@
+@extends('admin.master')
+
+@section('title')
+    <title>Category Page</title>
+@endsection
+
+@section('content')
+    
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ProductController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::resource('category', CategoryController::class);
+Route::resource('product', ProductController::class);
